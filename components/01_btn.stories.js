201dfe9044eb05249f48bea22456713029cc22f7
kk-shindo <kk-shindo@respect-pal.jp>
2020-06-30 18:39:15 +0900

    ボタンコンポーネントの登録
    preview.jsにてgulpで出力されたstyle.cssの読み込み

---
+++ components/01_btn.stories.js
@@ -0,0 +1,73 @@
+export default {
+  title: '01_Button'
+}
+
+export const C_Button1 = () => {
+  return (`
+    <div class="c-btn1">
+      <a href="">DummyDummy</a>
+    </div>
+  `)
+}
+
+export const C_Button1_Color1 = () => {
+  return (`
+    <div class="c-btn1 c-btn1--color1">
+      <a href="">DummyDummy</a>
+    </div>
+  `)
+}
+
+export const C_Button1_Color2 = () => {
+  return (`
+    <div class="c-btn1 c-btn1--color2">
+      <a href="">DummyDummy</a>
+    </div>
+  `)
+}
+
+export const L_Button1_Center = () => {
+  return (`
+    <div class="l-btn1 l-btn1--center">
+      <div class="c-btn1">
+        <a href="">DummyDummy</a>
+      </div>
+    </div>
+  `)
+}
+
+export const L_Button1_Right = () => {
+  return (`
+    <div class="l-btn1 l-btn1--right">
+      <div class="c-btn1">
+        <a href="">DummyDummy</a>
+      </div>
+    </div>
+  `)
+}
+
+export const L_Button1_2BtnCenter = () => {
+  return (`
+    <div class="l-btn1 l-btn1--2btnCenter">
+      <div class="c-btn1">
+        <a href="">DummyDummy</a>
+      </div>
+      <div class="c-btn1 c-btn1--color1">
+        <a href="">DummyDummy</a>
+      </div>
+    </div>
+  `)
+}
+
+export const LButton1_2BtnBetween = () => {
+  return (`
+    <div class="l-btn1 l-btn1--2btnBetween">
+      <div class="c-btn1">
+        <a href="">DummyDummy</a>
+      </div>
+      <div class="c-btn1 c-btn1--color1">
+        <a href="">DummyDummy</a>
+      </div>
+    </div>
+  `)
+}
